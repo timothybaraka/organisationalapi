5bf82a8af817c1db1cfebbc7aa77e8e989c5cc22
timothybaraka <timothybaraka44@gmail.com>
2020-02-20 08:53:19 +0300

    Add a codebeat grade to the readme

--- readme.md
+++ readme.md
@@ -3,11 +3,16 @@ This is a java app that is used to organise information in an organisation
 in terms of departments, News regarding the organisation and users(employees) data.
 
 ## Author
-Timothy Baraka 15 February 2020
+Timothy Baraka 18 February 2020
 
 ## Getting Started
 Clone or download this repo into your machine to get started.
+
 Install postman on your machine where the front end will be displayed.
+
+Build your project then run it on the terminal.
+
+Open postman and run the link http//localhost:4567
 
 ## Requirements
 IDE - Prefarably IntelliJ
@@ -19,6 +24,7 @@ Java - the language.
 Intellij - IDE
 
 ## CodeBeat score
+[![codebeat badge](https://codebeat.co/badges/7abd084c-8f23-4aae-b39d-19c49b1ddfa6)](https://codebeat.co/projects/github-com-timothybaraka-organisationalapi-dev)
 
 
 ## Licence
--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,6 +3,8 @@ import dao.Sql2oNewsDao;
 import dao.Sql2oDepartmentsDao;
 import dao.Sql2oUsersDao;
 import models.Departments;
+import models.News;
+import models.Users;
 import org.sql2o.Connection;
 import org.sql2o.Sql2o;
 import static spark.Spark.*;
@@ -32,5 +34,69 @@ public class App {
             res.type("application/json");
             return gson.toJson(departments);
         });
+
+
+        post("/departments/new", "application/json", (req, res) -> {
+            Departments departments = gson.fromJson(req.body(), Departments.class);
+            departmentsDao.add(departments);
+            res.status(201);
+            return gson.toJson(departments);
+        });
+//        get: show all existing departments
+        get("/departments", "application/json", (req, res) -> {
+            return gson.toJson(departmentsDao.getAll());
+        });
+//        get: show specific department by id
+        get("/departments/:id", "application/json", (req, res) -> {
+            int departmentId = Integer.parseInt(req.params("id"));
+            return gson.toJson(departmentsDao.findById(departmentId));
+        });
+//        post: add new employee
+        post("/users/new", "application/json", (req, res) -> {
+            Users users = gson.fromJson(req.body(), Users.class);
+            usersDao.add(users);
+            res.status(201);
+            return gson.toJson(users);
+        });
+//        get: show all users regardless of department
+        get("/employees", "application/json", (req, res)->{
+            return gson.toJson(usersDao.getAll());
+        });
+
+        get("/users/:id", "application/json", (req, res) -> {
+            int employeeId = Integer.parseInt(req.params("id"));
+            return gson.toJson(usersDao.findId(usersId));
+        });
+//        filters
+        after((req, res) ->{
+            res.type("application/json");
+        });
+        //        get: show all employees in a specific department
+        get("/department/:deptId", "application/json", (req, res) ->{
+//            int deptId = Integer.parseInt("deptId");
+            int deptId = Integer.parseInt(req.queryParams("deptId"));
+            Users users = gson.fromJson(req.body(), Users.class);
+            departments.setDeptId(deptId);
+            departmentsDao.getAllEmployeesInADept(deptId);
+            res.status(201);
+            return gson.toJson(users);
+        });
+//        post: create news
+        post("/news/new", "application/json", (req, res) -> {
+            News news = gson.fromJson(req.body(), News.class);
+            newsDao.add(news);
+            res.status(201);
+            return gson.toJson(news);
+        });
+//        get: show all generalNews
+        get("/news", "application/json", (req, res)->{
+            return gson.toJson(newsDao.getGeneralNews());
+        });
+
+//        get: show all departmentNews
+        get("/departmentNews", "application/json", (req, res)->{
+            return gson.toJson(departments.getDepartmentsNews());
+        });
+    }
     }
 }
